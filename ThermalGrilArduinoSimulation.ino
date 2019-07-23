7dce3336ef035c417c82311db4ca7cf7cffdde31
AidanYtgr <53199423+AidanYtgr@users.noreply.github.com>
2019-07-22 17:06:36 -0700

    Program to simulate thermal grill serial data

---
+++ ThermalGrilArduinoSimulation.ino
@@ -0,0 +1,14 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.println("B:");
+  Serial.println("A:");
+}
+
+void loop() {
+  //delay(100);
+  //int r = rand() % 20;
+  //Serial.println("A:");
+  //r = rand() % 20;
+  //Serial.println("B:");
+
+}
